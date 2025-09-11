72339c2c90887f6db6c8d004f73c619e99ede933
Ajaythodeti77 <thodetiajayyadav@gmail.com>
2025-09-11 14:09:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your JS code here. If required.
+
+		const form = document.getElementById("form")
+		const Boxes = document.querySelectorAll(".grid-items")
+		form.addEventListener("submit",(e)=>{
+			BgColor();
+    e.preventDefault();   
+    const selectBox = document.getElementById("selectBox").value;
+    const entercolor = document.getElementById("entercolor").value;
+    const box = document.getElementById(selectBox);
+    if(box){
+        box.style.backgroundColor = entercolor;
+    }else{
+        alert("Please enter a valid box number between 1 to 9")
+    }
+})
+function BgColor(){
+for(box of Boxes){
+    box.style.backgroundColor = "Transparent"
+}
+}
